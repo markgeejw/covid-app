058d4522326dc08d7772158badd04a22a4299bf0
worulz <leon.truong1@gmail.com>
2020-03-24 00:33:21 +1100

    sample

--- client/src/components/Chart.js
+++ client/src/components/Chart.js
@@ -1,20 +1,26 @@
 import React, { Component } from 'react';
 import Highcharts from 'highcharts';
 import HighchartsReact from 'highcharts-react-official';
+//import HighchartsReactNative from '@highcharts/highcharts-react-native';
 import { Container } from 'react-bootstrap';
 
-
+require("highcharts/modules/data")(Highcharts);
 
 export default class Chart extends Component {
   render() {
     const options = {
-      
         title: {
             text: 'VIC'
         },
         data: {
-            googleSpreadsheetKey: '12Ldo5cwQMCu1Aka2p0nCCvajjYlXQ_ags1OtwHOyKeM'
+            googleSpreadsheetKey: '12Ldo5cwQMCu1Aka2p0nCCvajjYlXQ_ags1OtwHOyKeM',
+            googleSpreadsheetWorksheet:1
         }
+        ,
+        series: [{
+          type: 'area'
+          // for each series, we can have a differnt line type
+        }]
     };
 
     return (
