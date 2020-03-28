8eaa85fb62a6264be98f540fb0218e5619658655
Khoa Cao <khoaa.cao@gmail.com>
2020-03-28 12:47:21 -0700

    Description changes

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -12,8 +12,8 @@ export default class Home extends Component {
             <h2>COVID-19 Model</h2>
             <div style={{ marginTop: 20, marginBottom: 20 }}>
                 <p>This epidemiological model examines the impact of different interventions on the number of patients infected, hospitalised and requiring ICU beds or ventilators over 6 months. It uses the most recent COVID-19 numbers, published and pre-published literature and health system data, which is updated daily. A thorough explanation of model parameter selection and implementation can be found <a href="https://docs.google.com/document/d/1kOWmbMYhcBRnckX7RuUgWDOY61zJXRseCN3iA-I5vT4/edit?usp=sharing">here</a>. Our model can be queried through our API here.</p>
-                <p>Disclaimer: This model should not be taken over expert public health or epidemiological advice. We take no responsibility for harm or actions resulting from this model. This software is provided under the MIT License. Copyright (C) 2020.</p>
-                <p>Model was created by <a href="https://www.linkedin.com/in/khoanguyencao/">Dr. Khoa Cao</a>, <a href="https://www.linkedin.com/in/mark-gee-64ab85166/">Mark Gee</a> and <a href="https://www.linkedin.com/in/leon-truong-16845840/">Leon Truong</a>.</p>
+                <p><strong>Disclaimer</strong>: This model should not be taken over expert public health or epidemiological advice. We take no responsibility for harm or actions resulting from this model. This software is provided under the MIT License. Copyright (C) 2020.</p>
+                <p><strong>Authors</strong>: Model was created by <a href="https://www.linkedin.com/in/khoanguyencao/">Dr. Khoa Cao</a>, <a href="https://www.linkedin.com/in/mark-gee-64ab85166/">Mark Gee</a> and <a href="https://www.linkedin.com/in/leon-truong-16845840/">Leon Truong</a>.</p>
             </div>
             <div style={{ marginTop: 20, marginBottom: 20 }}>
             <h4>List of supported countries and states:</h4>
