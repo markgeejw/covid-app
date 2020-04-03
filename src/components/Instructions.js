d6fe77d1460f2e4fdbed5df8bbee5c2f56d36e00
Khoa Cao <khoaa.cao@gmail.com>
2020-04-03 13:49:39 -0700

    About page updated

--- src/components/Instructions.js
+++ src/components/Instructions.js
@@ -15,7 +15,6 @@ export default class Instructions extends Component {
                         <Col style={{ textAlign: "left", paddingTop: 20 }}>
                             <p>Select the number of weeks you want for each intervention (max 26 weeks). Hover over each to find out what they mean and what measures are required.</p>
 		                    <p>Interventions occur in order, from least invasive to most invasive (top to bottom). If you don't reach 26 weeks, the rest becomes social distancing.</p>
-                            <p>The simplest explanation of R0 is an R0 of 2 means one infected person will infect two people on average. Click <a href="https://www.theatlantic.com/science/archive/2020/01/how-fast-and-far-will-new-coronavirus-spread/605632/">here</a> for a more thorough explanation.</p>
                             <p>Click on model parameters to change more detailed variables, like how many ventilators are available.</p>
                         </Col>
                     </Row>
