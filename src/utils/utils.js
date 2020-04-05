691c45dfc5d11a1adcc5b4bbec53770c08ecf16d
Khoa Cao <khoaa.cao@gmail.com>
2020-04-05 13:01:36 -0700

    Fixed USA name typo

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -2,6 +2,6 @@ export function format_countries(country) {
   if (country === "Congo (Brazzaville)") return "Congo (Republic)";
   else if (country === "Congo (Kinshasa)") return "Congo (Democratic Republic)";
   else if (country === "Korea, South") return "South Korea";
-  else if (country === "US") return "United State of America";
+  else if (country === "US") return "United States of America";
   else return country;
 }
